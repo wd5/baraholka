26c6478614050cac2778715d1715998292cdbdef
Vasiliy Korchagin <vasiliy.korchagin@gmail.com>
2012-04-17 22:24:01 +0400

    Haystack updated to 1.2.7.

--- buy/haystack/views.py
+++ buy/haystack/views.py
@@ -90,18 +90,30 @@ class SearchView(object):
     def build_page(self):
         """
         Paginates the results appropriately.
-        
+
         In case someone does not want to use Django's built-in pagination, it
         should be a simple matter to override this method to do what they would
         like.
         """
-        paginator = Paginator(self.results, self.results_per_page)
-        
+
         try:
-            page = paginator.page(self.request.GET.get('page', 1))
+            page_no = int(self.request.GET.get('page', 1))
+        except (TypeError, ValueError):
+            raise Http404("Not a valid number for page.")
+
+        if page_no < 1:
+            raise Http404("Pages should be 1 or greater.")
+
+        start_offset = (page_no - 1) * self.results_per_page
+        self.results[start_offset:start_offset + self.results_per_page]
+
+        paginator = Paginator(self.results, self.results_per_page)
+
+        try:
+            page = paginator.page(page_no)
         except InvalidPage:
-            raise Http404
-        
+            raise Http404("No such page!")
+
         return (paginator, page)
     
     def extra_context(self):
